723b392baa08336452414900dfdec4b64db45daf
Tawfik Ahmed <tawfikahmed1x@gmail.com>
2026-03-07 13:37:19 +0200

    initialize project and database

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UserModule,
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DATABASE_HOST'),
+        port: config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DATABASE_USERNAME'),
+        database: config.get<string>('DATABASE_NAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('api/users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+}
